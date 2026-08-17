c1bb167fe87cb73142d646149f8bce84026eb01c
SHREYA GUPTA <akshreyagupta28@gmail.com>
2026-08-17 14:33:28 +0530

    Time: 2 ms (14.92%), Space: 9.9 MB (8.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            char c=s[i];
+            if(c=='(')
+            st.push(')');
+            else if (c=='{')
+            st.push('}');
+            else if(c =='[')
+            st.push(']');
+            else if(st.empty() || st.top()!=c)
+            return false;
+
+    else
+    st.pop();
+
+        }
+        return st.empty();
+    }
+};
